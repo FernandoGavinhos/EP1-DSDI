369612679525a61ee95b8fb49786305effbaa87e
Fernando Santos <Fernando Santos>
2016-04-02 10:51:44 -0300

    novas interfaces{Part; PartRepository}; nova classe{CList}

---
+++ src/br/com/partRepository/interfaces/Part.java
@@ -0,0 +1,18 @@
+package br.com.partRepository.interfaces;
+
+import br.com.partRepository.classes.CList;
+
+public interface Part {
+	
+	//getters
+	public int getPartId();
+	public String getPartName();
+	public String getPartInfo();
+	public CList getComponentList();
+
+	//setters
+	public void setPartName(String partName);
+	public void setPartInfo(String partInfo);
+	public void AddComponent(Part Component);
+	
+}
---
+++ src/br/com/partRepository/interfaces/PartRepository.java
@@ -0,0 +1,5 @@
+package br.com.partRepository.interfaces;
+
+public interface PartRepository {
+
+}
