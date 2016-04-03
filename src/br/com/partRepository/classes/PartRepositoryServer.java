9efa6f765bc9614ef3aa1a263d747fd240054725
Fernando Santos <Fernando Santos>
2016-04-03 20:21:41 -0300

    classes cliente e servidor impementadas

---
+++ src/br/com/partRepository/classes/PartRepositoryServer.java
@@ -0,0 +1,88 @@
+package br.com.partRepository.classes;
+
+import java.rmi.RemoteException;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.UUID;
+
+import br.com.partRepository.interfaces.*;
+
+public class PartRepositoryServer implements PartRepository, Part {
+
+	private String serverName;
+	private LinkedList<PartRepositoryClient> clients;
+	
+	public PartRepositoryServer(String serverName){
+		this.serverName = serverName;
+	}
+
+	@Override
+	public UUID getPartId() {
+		return null;
+	}
+
+	@Override
+	public String getPartName() {
+		return null;
+	}
+
+	@Override
+	public String getPartInfo() {
+		return null;
+	}
+
+	@Override
+	public CList getComponentList() {
+		return null;
+	}
+
+	@Override
+	public void setPartName(String partName) {
+		
+	}
+
+	@Override
+	public void setPartInfo(String partInfo) {
+		
+	}
+
+	@Override
+	public void AddComponent(Part Component, int quant) {
+		
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public Set<Part> getComponents() {
+		return null;
+	}
+
+	@Override
+	public void setName(String repoName) {
+		
+	}
+
+	@Override
+	public void setComponents(Set<Part> components) {
+		
+	}
+
+	@Override
+	public boolean addPart(Part component) throws RemoteException {
+		return false;
+	}
+
+	@Override
+	public Part getPart(Part component) throws RemoteException {
+		return null;
+	}
+
+	@Override
+	public Set<Part> listAll() throws RemoteException {
+		return null;
+	}
+}
