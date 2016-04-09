c039e695889e125af1cae138313f7183f5796365
ghn1712 <ghn1712@gmail.com>
2016-04-09 18:06:36 -0300

    Primeiro teste

---
+++ src/br/com/partRepository/client/Teste.java
@@ -0,0 +1,38 @@
+package br.com.partRepository.client;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Iterator;
+import java.util.Set;
+
+import br.com.partRepository.classes.Piece;
+import br.com.partRepository.interfaces.Part;
+import br.com.partRepository.interfaces.PartRepository;
+
+public class Teste {
+	public static void main(String[]args){
+		if (System.getSecurityManager() == null){
+			System.setSecurityManager(new SecurityManager());
+		}
+		try {
+			String name = "Teste";
+			Registry registry = LocateRegistry.getRegistry("TODO");
+			PartRepository p = (PartRepository) registry.lookup(name);
+			Piece part = new Piece();
+			part.setPartInfo("daskdklada");
+			part.setPartName("lodoaskda");
+			boolean addOk = p.addPart(part);
+			if(addOk){
+				System.out.println("Adicionou a part "+part);
+				Set<Part> comp = p.getComponents();
+				Iterator<Part> it = comp.iterator();
+				while(it.hasNext()){
+					Part aux = it.next();
+					System.out.println(aux);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
